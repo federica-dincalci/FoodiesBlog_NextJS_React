4aa08b6a7c58ce9c541f142ff62140a6b08afb9d
Federica d'Incalci <federica19971@hotmail.it>
2025-10-08 21:06:27 +0200

    Completed the details page

---
+++ app/meals/not-found.js
@@ -0,0 +1,8 @@
+export default function() {
+    return ( 
+        <main className="not-found">
+            <h1>Meal not found</h1>
+            <p>Unfortunately, we couldn't find the requested page or meal data.</p>
+        </main>
+    );
+}
